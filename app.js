9f9f8d229af067122d5ac68a80a4e65d07fa95c3
nuttapon <dev.nuttapon@gmail.com>
2022-06-24 10:34:45 +0700

    no message

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const app = express()
 const port = 3000
 
 app.set ( "view engine", "ejs" );
+app.set('views', 'views');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/page', (req, res) => {
     res.render ( "homepage", 
     { 
-        title: "I just code IT [Youtube Channel]"
-        , detail: "Hello EJS module from I just code IT."
+        title: "I just code IT [Youtube Channel]", 
+        detail: "Hello EJS module from I just code IT."
     } 
 );
   })
@@ -20,5 +17,5 @@ app.get('/page', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
